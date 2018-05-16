27901e58f01ac8c85f20c394910805e67fdd28dd
Wojciech Kalebasiak <kalebasiakw@gmail.com>
2018-05-16 17:13:54 +0200

    Add sockets configuration

--- index.js
+++ index.js
@@ -11,3 +11,35 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+app.get('/', function (req,res) {
+   res.render('start'); 
+});
+io.on('connection', function (socket) {
+    userService.addUser({
+    id:socket.id,
+    name
+    });
+    io.emit('update', {
+        users: userService.getAllUsers()
+    });
+});
+io.on('connection', function (socket) {
+   socket.on('disconnect', ()=> {
+        userService.removeUser(socket.id);
+        socket.broadcast.emit('update', {
+            users:userService.getAllUsers()
+        });
+   });
+});
+io.on('connection', function (socket) {
+    socket.on('message', (message)=>{
+        const {name} = user.getUserById(socket.id);
+        socket.broadcast.emit('message', {
+            text:message.text,
+            from: name
+        });
+    });
+});
+server.listen(3000, function() {
+    console.log('Listening on http://localhost:3000');
+});
