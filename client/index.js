7682879a09fe3093074c34627062b709a9a8cd99
Wojciech Kalebasiak <kalebasiakw@gmail.com>
2018-05-16 22:01:16 +0200

    Add index.js

---
+++ client/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReacDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+import App from './App';
+
+const render = (Component) => {
+    ReacDOM.render(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        document.getElementById('root')
+    );
+};
+
+render(App)
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        const NewApp = require('./App').default;
+        render(NewApp);
+    });
+}
